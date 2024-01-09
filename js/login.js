d950099a21f90489cc8e770c9f799ab40b5b6179
VICTOR-M8 <darkhink8@gmail.com>
2024-01-09 20:15:38 +0100

    new changes

---
+++ js/login.js
@@ -0,0 +1,18 @@
+const loginForm = document.querySelector('#loginForm');
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const email = document.querySelector('#email').value;
+            const password = document.querySelector('#password').value;
+            const Users = JSON.parse(localStorage.getItem('users')) || [];
+            const validUser = Users.find(user => user.email === email && user.password === password);
+            if (!validUser) {
+                alert('Usuario y/o contraseña incorrectos');
+            } else {
+                alert(`¡Bienvenido ${validUser.name}!`);
+
+                localStorage.setItem('usuario', JSON.stringify(validUser));
+                
+                window.location.href = 'list.html';
+
+            }
+        });
